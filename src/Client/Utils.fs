e31ada8c1f27afca33fe2f4d05b8f66315f3538d
Julien Roncaglia <julien@roncaglia.fr>
2019-08-31 21:49:43 +0200

    Add NoMemoization to utils

--- src/Client/Utils.fs
+++ src/Client/Utils.fs
@@ -6,8 +6,10 @@ open Fable.React
 type ComponentEquality =
     | ByRef
     | ByValue
+    | NoMemoization
 
 let inline elmishView name equality render =
     match equality with
     | ByValue -> FunctionComponent.Of(render, name, equalsButFunctions)
     | ByRef -> FunctionComponent.Of(render, name, memoEqualsButFunctions)
+    | NoMemoization -> FunctionComponent.Of(render, name)
