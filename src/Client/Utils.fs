70a4fc82932928df544454baf05234dd5c079ef1
Julien Roncaglia <julien@roncaglia.fr>
2019-08-31 16:31:26 +0200

    Add functionComponent

--- src/Client/Utils.fs
+++ src/Client/Utils.fs
@@ -2,4 +2,5 @@ module Utils
 
 open Fable.React
 
+let inline functionComponent name render = FunctionComponent.Of(render, name)
 let inline elmishView name render = FunctionComponent.Of(render, name, equalsButFunctions)
