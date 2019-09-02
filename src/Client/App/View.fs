b921047290b7c8df4102d71412b4aaf6ff4283a1
Julien Roncaglia <julien@roncaglia.fr>
2019-09-02 17:50:29 +0200

    Step 2: Introducing components

--- src/Client/App/View.fs
+++ src/Client/App/View.fs
@@ -7,19 +7,32 @@ open Connection.Types
 open Fable.React
 open Props
 
+type RootProps = {
+    model: App.Types.Model
+    dispatch: App.Types.Msg -> unit
+}
+
+let mainArea = elmishView "MainArea" NoMemoization <| fun { model = model; dispatch = dispatch; } ->
+    div [ ClassName "col-xs-12 col-md-8 fs-chat" ]
+        [
+            (match model.currentPage, model.chat with
+            | Route.Overview, _ -> Overview.View.root
+
+            | Channel chan, Connected { serverData = { Channels = channels } } when channels |> Map.containsKey chan ->
+                let dispatchChannelMessage m = ChatServer.Types.ChannelMsg(chan, m) |> ApplicationMsg |> ChatDataMsg |> dispatch
+                fragment [] (Channel.View.root channels.[chan] dispatchChannelMessage)
+
+            | _ ->
+                div [] [ str "bad channel route" ])
+        ]
+
 let root model dispatch =
-    let mainArea =
-        match model.currentPage, model.chat with
-        | Route.Overview, _ -> [ Overview.View.root ]
+    let menu = NavMenu.View.menu {
+        chatData = model.chat
+        currentPage = model.currentPage
+        dispatch = (ApplicationMsg >> ChatDataMsg >> dispatch)
+    }
 
-        | Channel chan, Connected { serverData = { Channels = channels } } when channels |> Map.containsKey chan ->
-            let dispatchChannelMessage m = ChatServer.Types.ChannelMsg(chan, m) |> ApplicationMsg |> ChatDataMsg |> dispatch
-            Channel.View.root channels.[chan] dispatchChannelMessage
-
-        | _ ->
-            [ div [] [ str "bad channel route" ] ]
-
-    div [ ClassName "container" ] 
-        [ div [ ClassName "col-md-4 fs-menu" ] 
-              (NavMenu.View.menu model.chat model.currentPage (ApplicationMsg >> ChatDataMsg >> dispatch))
-          div [ ClassName "col-xs-12 col-md-8 fs-chat" ] mainArea ]
+    div [ ClassName "container" ]
+        [ menu
+          mainArea { model = model; dispatch = dispatch; }]
--- src/Client/NavMenu/View.fs
+++ src/Client/NavMenu/View.fs
@@ -10,68 +10,123 @@ open Channel.Types
 open ChatServer.Types
 open Connection.Types
 
-let menuItem htmlProp name topic isCurrent =
+let private menuItem htmlProp name topic isCurrent =
     button
       [ classList [ "btn", true; "fs-channel", true; "selected", isCurrent ]
         htmlProp ]
       [ h1 [] [str name]
         span [] [str topic]]
 
-let menuItemChannel (ch: ChannelInfo) currentPage = 
+type private MenuItemChannelProps = {
+  key: string
+  ch: ChannelInfo
+  currentPage: Route
+}
+
+let private menuItemChannel = elmishView "ChannelMenuItem" ByRef <| fun { ch = ch; currentPage = currentPage } ->
     let targetRoute = Channel ch.Id
     let jump _ = document.location.hash <- toHash targetRoute
     menuItem (OnClick jump) ch.Name ch.Topic (targetRoute = currentPage)
 
-let menuItemChannelJoin dispatch = 
+type private MenuItemChannelJoinProps = {
+  key: string
+  ch: ChannelInfo
+  dispatch: ChatServer.Types.Msg -> unit
+}
+
+let private menuItemChannelJoin = elmishView "ChannelJoinMenuItem" ByRef <| fun { ch = ch; dispatch = dispatch } ->
     let join chid _ = chid |> Join |> dispatch
-    fun (ch: ChannelInfo) ->
-      menuItem (OnClick <| join ch.Id) ch.Name ch.Topic false
+    menuItem (OnClick <| join ch.Id) ch.Name ch.Topic false
 
-let menu (chatData: Model) currentPage dispatch =
-    match chatData with
-    | NotConnected ->
-      [ div [] [str "not connected"] ]
-    | Connecting _ ->
-      [ div [] [str "connecting"] ]
-    | Connected { serverData = { Me = me; NewChanName = newChanName; Channels = channels; ChannelList = channelList } } ->
-      let opened, newChanName = newChanName |> function |Some text -> (true, text) |None -> (false, "")
-      [ yield div
-          [ ClassName "fs-user" ]
-          [ UserAvatar.View.root me.ImageUrl
-            h3 [Id "usernick"] [str me.Nick]
-            span [Id "userstatus"] [ str me.Status]
-            button
-              [ Id "logout"; ClassName "btn"; Title "Logout"
-                OnClick (fun _ -> document.location.href <- "/logoff") ]
-              [ i [ ClassName "mdi mdi-logout-variant"] [] ]
-           ]
-        yield h2 []
-          [ str "My Channels"
-            button
-              [ ClassName "btn"; Title "Create New"
-                OnClick (fun _ -> (if opened then None else Some "") |> (SetNewChanName >> dispatch)) ]
-              [ i [ classList [ "mdi", true; "mdi-close", opened; "mdi-plus", not opened ] ] []]
-          ]
-        yield input
-          [ Type "text"
-            classList ["fs-new-channel", true; "open", opened]
-            Placeholder "Type the channel name here..."
-            DefaultValue newChanName
-            AutoFocus true
-            OnChange (fun ev -> !!ev.target?value |> (Some >> SetNewChanName >> dispatch) )
-            OnKeyPress (fun ev -> if !!ev.which = 13 || !!ev.keyCode = 13 then dispatch CreateJoin)
-            ]
+type private UserProps = {
+  me: UserInfo
+}
 
-        for (_, ch) in channels |> Map.toSeq do
-          yield menuItemChannel ch.Info currentPage
+let private user = elmishView "User" ByRef <| fun { me = me} ->
+    div
+        [ ClassName "fs-user" ]
+        [ UserAvatar.View.root me.ImageUrl
+          h3 [Id "usernick"] [str me.Nick]
+          span [Id "userstatus"] [ str me.Status]
+          button
+            [ Id "logout"; ClassName "btn"; Title "Logout"
+              OnClick (fun _ -> document.location.href <- "/logoff") ]
+            [ i [ ClassName "mdi mdi-logout-variant"] [] ]
+         ]
 
-        yield h2 []
-            [ str "All Channels"
-              button
-                [ ClassName "btn"; Title "Search" ]
-                [ i [ ClassName "mdi mdi-magnify" ] []]
-            ]
+type private UserChannelsProps = {
+    channels: Map<ChannelId, Channel.Types.Model>
+    newChanName: string option
+    currentPage: Route
+    dispatch: ChatServer.Types.Msg -> unit
+}
+
+let private userChannels = elmishView "UserChannels" ByValue <| fun { newChanName = newChanName; channels = channels; currentPage = currentPage; dispatch = dispatch } ->
+    let opened, newChanName = newChanName |> function |Some text -> (true, text) |None -> (false, "")
+
+    let channels = [|
+      for (_, ch) in channels |> Map.toSeq do
+        yield menuItemChannel { ch = ch.Info; currentPage = currentPage; key = ch.Info.Id }
+    |]
+    fragment [] [
+      h2 []
+        [ str "My Channels"
+          button
+            [ ClassName "btn"; Title "Create New"
+              OnClick (fun _ -> (if opened then None else Some "") |> (SetNewChanName >> dispatch)) ]
+            [ i [ classList [ "mdi", true; "mdi-close", opened; "mdi-plus", not opened ] ] []]
+        ]
+      input
+        [ Type "text"
+          classList ["fs-new-channel", true; "open", opened]
+          Placeholder "Type the channel name here..."
+          DefaultValue newChanName
+          AutoFocus true
+          OnChange (fun ev -> !!ev.target?value |> (Some >> SetNewChanName >> dispatch) )
+          OnKeyPress (fun ev -> if !!ev.which = 13 || !!ev.keyCode = 13 then dispatch CreateJoin)
+        ]
+      channels |> ofArray
+    ]
+
+type private AllChannelsProps = {
+    channels: Map<ChannelId, Channel.Types.Model>
+    channelList: Map<ChannelId, ChannelInfo>
+    dispatch: ChatServer.Types.Msg -> unit
+}
+
+let private allChannels = elmishView "AllChannels" ByValue <| fun { channels = channels; channelList = channelList; dispatch = dispatch } ->
+    let channelList = [|
         for (chid, ch) in channelList |> Map.toSeq do
             if not(channels |> Map.containsKey chid) then
-                yield menuItemChannelJoin dispatch ch
-      ]
+                yield menuItemChannelJoin { dispatch = dispatch; ch = ch; key = ch.Id }
+    |]
+    fragment [] [
+      h2 []
+          [ str "All Channels"
+            button
+              [ ClassName "btn"; Title "Search" ]
+              [ i [ ClassName "mdi mdi-magnify" ] []]
+          ]
+      channelList |> ofArray
+    ]
+
+type MenuProps = {
+    chatData: Model
+    currentPage: Route
+    dispatch: ChatServer.Types.Msg -> unit
+}
+
+let menu = elmishView "Menu" ByValue <| fun { chatData = chatData; currentPage = currentPage; dispatch = dispatch } ->
+    div
+        [ ClassName "col-md-4 fs-menu" ]
+        (match chatData with
+        | NotConnected ->
+            [ div [] [str "not connected"] ]
+        | Connecting _ ->
+            [ div [] [str "connecting"] ]
+        | Connected { serverData = { Me = me; NewChanName = newChanName; Channels = channels; ChannelList = channelList } } ->
+            [
+                user { me = me }
+                userChannels { newChanName = newChanName; channels = channels; currentPage = currentPage; dispatch = dispatch }
+                allChannels { channels = channels; channelList = channelList; dispatch = dispatch }
+            ])
